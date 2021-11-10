4ee4a54e6b3920a11b4876586db5858fcfc33835
mptak12 <56129259+mptak12@users.noreply.github.com>
2021-11-10 21:24:09 +0100

    secret chamber - update

--- gra.cpp
+++ gra.cpp
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <windows.h>
 #include <conio.h>
-
+#include <random> //do mersena
 using std::vector, std::cout, std::cin, std::setw, std::endl;
 using std::fstream, std::ostream;
 
@@ -20,6 +20,12 @@ ostream& operator<< (ostream& os, VDD m) {
 		os << endl;
 	}
 	return os;
+}
+
+int twist(int a, int b) {
+	std::mt19937	twister(rand());
+	std::uniform_int_distribution<int> dist(a, b);
+	return dist(twister);
 }
 
 void player(int p[],VDD& m) {//int p, VDD& m*/
@@ -57,24 +63,32 @@ void player(int p[],VDD& m) {//int p, VDD& m*/
 void treas(int p[], int tpos[], VDD& m)//losuje pozycje skarbu
 {
 	m[tpos[1]][tpos[0]] = '.';
-	//cout << tpos[1] << " " << tpos[0] << endl;
+	/*
 	tpos[0] = rand()%10;
 	tpos[1] = rand()%10;
-
-	if (p[0] == tpos[0] && p[1] == tpos[1]) tpos[0] = rand()%10;
+	*/
+	tpos[0] = twist(0, 9);
+	tpos[1] = twist(0, 9);
+	if (p[0] == tpos[0] && p[1] == tpos[1]) tpos[0] = twist(0, 9);
 	m[tpos[1]][tpos[0]] = 'T';
 }
 
 void trap(int trap1[], int tpos[], VDD& m)//, int trap2[]) //losujemy pulapki
 {
 	m[trap1[1]][trap1[0]] = '.';
-	trap1[0] += rand()%3-1;
-	if (trap1[0] < 0) trap1[0]++;
-	trap1[1] += rand()%3-1;
+	//trap1[0] += rand()%3-1;
+	trap1[0] += twist(-1, 1);
+	if (trap1[0] < 0) trap1[0]++; //zabezpieczenia przed wyjsciami z planszy
+	if (trap1[0] > 8) trap1[0]--;
+	trap1[1] += twist(-1, 1);
 	if (trap1[1] < 0) trap1[1]++;
-	if (trap1[0] == tpos[0] && trap1[1] == tpos[1]) trap1[1]++; //prowizorka jak sie pozycje pokryja
+	if (trap1[1] > 8) trap1[1]--;
+	
+	if (trap1[0] == tpos[0] && trap1[1] == tpos[1] ) trap1[1]++; //prowizorka jak sie pozycje pokryja || trap1[1]<8
+	//else if (trap1[0] == tpos[0] && trap1[1] == tpos[1] || trap1[1] > 8) trap1[1]--;
 	else
 		m[trap1[1]][trap1[0]] = 'X';
+	
 }
 
 int main()
@@ -85,7 +99,7 @@ int main()
 	int tpos[2] = { 1,1 }; //pozycja skarbu
 	int trap1[2] = { 0,0 }, trap2[2] = { 9,9 };
 	int pts =0;
-	int time = 500;
+	int time = 300;
 	cout << "Click 'w' to start" << endl;
 	treas(pos, tpos, map);
 	while (1) {
@@ -101,17 +115,19 @@ int main()
 		if (trap1[0] == pos[0] && trap1[1] == pos[1]) 
 		{
 			for (int i = 0; i < 5; i++) { //jak sie spotkaja
+				system("cls");
 				map[pos[1]][pos[0]] = 'X';
 				cout << map;
-				Sleep(500);
+				Sleep(200);
 				system("cls");
 				map[pos[1]][pos[0]] = '+';
 				cout << map;
-				Sleep(500);
+				Sleep(200);
 				system("cls");
 			}
 			break;
 		}
+		//cout << time;
 		Sleep(time); //czas w ms
 		time--;
 		system("cls");
