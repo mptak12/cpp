a8e263c7f2f2c5f7d915753472e97c383c5a6b01
mptak12 <56129259+mptak12@users.noreply.github.com>
2021-11-18 11:30:56 +0100

    copy, sort, lambda, wyswietlanie update

--- lambda_sort.cpp
+++ lambda_sort.cpp
@@ -4,9 +4,11 @@
 #include <random>
 #include <xutility>
 #include <iterator>
-
+#include <string>
 using std::vector, std::cout, std::endl;
 using std::generate, std::copy, std::ostream_iterator, std::sort;
+using std::string;
+using VDD = vector<vector<int>>;
 
 int main()
 {
@@ -16,9 +18,31 @@ int main()
 	generate(los.begin(), los.end(), []() {return rand() % 15; }); //tu jest ju¿ f.lambda co generuje rand
 	copy(los.begin(), los.end(), ostream_iterator<int>(cout," ")); //wypisanie na ekran
 
-	sort(los.begin(), los.end(), [](int a, int b) {return a > b;}); //sortowanie z lambd¹
+	sort(los.begin(), los.end(), [](int a, int b) {return a < b;}); //sortowanie z lambd¹
 	cout << endl;
 	for (const auto x : los) cout << x << " "; //wypisanie wektora
+
+	/*
+	VDD matrix(10, vector(10, 1));
+	for (int row = 0; row < 10; row++) {
+		for (int col = 0; col < 10; col++) {
+			cout << matrix[row][col] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+	for (int row = 0; row < 10; row++) {
+		copy(matrix[0].begin(), matrix[0].end(), ostream_iterator<int>(cout, " "));
+		cout << endl;
+	}
+	
+	copy_if(los.begin(), los.end(),
+		std::ostream_iterator<int>(std::cout, " "),
+		[](int x) { return x % 2 != 0; });
+	*/
+	string A = "abc";
+	string B;
+	reverse_copy(A.begin(), A.end(), std::back_inserter(B));
 
 	return 0;
 }
