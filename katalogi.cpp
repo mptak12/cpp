dd1caeef62a00a367cfa851ca6d84c1e831acf1a
mptak12 <56129259+mptak12@users.noreply.github.com>
2022-02-02 17:46:15 +0100

    Counts how many letters your files contain

--- katalogi.cpp
+++ katalogi.cpp
@@ -5,7 +5,6 @@
 #include <fstream>
 #include <Windows.h>
 
-
 using std::cout, std::cin, std::vector, std::wstring, std::string, std::wcout, std::wcin, std::endl, std::fstream;
 using std::ofstream; //tylko do zapisu
 using std::setw;
@@ -13,6 +12,10 @@ namespace fs = std::filesystem;
 using vect = vector<wstring>;
 
 class reader {
+/// <summary>
+/// this class is iterating through filesystem
+/// variable path is uppdated every time a file matches to extension pattern stored in "flags"
+/// </summary>
     const fs::path curr_path{ fs::current_path() }; //zmienna z aktualna sciezka
     vect paths{}; //tu skladowane sa sciezki do plikow
     wstring src_path; //tu zaczynamy analize
@@ -23,37 +26,33 @@ public:
         else src_path=wstring(addr.begin(), addr.end()); //konwersja z string na wstring
     }
 
-    vect read(); //glowna funkcja z klasy
+    void read(); //glowna funkcja z klasy
     auto set_paths(vect p) { paths = p; }
     auto get_paths() const { return paths; }
 };
 
-vect reader::read()//wstring pathToShow) 
+void reader::read()//wstring pathToShow) 
 {
-    vect pa, temp; //wektory potrzebne do rekurencji
-
-        for (const auto& entry : fs::directory_iterator(src_path)) { //zmienna entry: tam iteruj¹ siê sciezki
-            const auto filenameStr = entry.path().filename().string();  //filenamestr = nazwa pliku
-            string ext = std::filesystem::path(entry).extension().string(); //zmienna z formatem pliku
-
-            if (ext.compare(".txt") == 0 || ext.compare(".cpp") == 0) {
-                //cout << "jest tekst lub cpp Nazwa: " << filenameStr << endl; //info ze znalazl plik
-                paths.push_back(entry.path());
-            }
-            
-            else if (entry.is_directory())
-            {  
-                src_path = entry.path();
-                temp = read(); //rekurencja
-                paths.insert(paths.end(), temp.begin(), temp.end()); //laczenie wektorow
-            }
+    const fs::directory_options options = (
+        fs::directory_options::skip_permission_denied
+        );
+    for (const auto& entry : fs::recursive_directory_iterator(src_path, options)) { //zmienna entry: tam iteruj¹ siê sciezki
+        if (entry.path().extension().string() == ".txt" || entry.path().extension().string() == ".cpp") {
+            paths.push_back(entry.path());
         }
-        return pa;
+    }
 }
 
 class scanner {
+/// <summary>
+/// this class opens vect variable from "reader" class
+/// then it counts files parameters
+/// parameters are passed to main MFC class through getters() 
+/// </summary>
     vect f_p{}; //od poprzedniej klasy - sciezki plikow
     int word_count{ 0 };
+    int letter_count{ 0 };
+    int line_count{ 0 };
     string all;
 
 public:
@@ -74,17 +73,30 @@ void scanner::if_empty_check() {
 
 void scanner::summary() {
     //ta funkcja skanuje wszystkie podane sciezki i podaje ich ogolne podsumowanie
-    string word;
+    string word, line;
     for (const auto a : f_p)
     {
-        fstream czytaj(a);
+        fstream czytaj(a); //bufor do wyrazow i liter
         while (czytaj >> word) {
-            word_count++;
+             word_count++;
             all += word;
         }
         czytaj.close();
+
+        fstream czytaj2(a); //bufor do liczby linii
+        while (!czytaj2.eof()) {
+            getline(czytaj2, line);
+            line_count++;
+            //cout << line;
+        }
+        //line_count--; //usuwam ostatni¹ pust¹ linijkê
+        czytaj2.close();
+
+        letter_count = all.length();
     }
     cout <<"Number of words in files : "<< word_count << endl;
+    cout << "LINES: " << line_count << endl;
+    cout << "LETTERS: " << letter_count << endl;
 }
 
 void scanner::letter_histogram() {
@@ -99,14 +111,13 @@ void scanner::letter_histogram() {
         }
     }
     cout <<"Letters in all files histogram"<< endl;
-    for (auto k{ 'a' }; k <= 'z'; ++k)
+    for (auto k{'a'}; k <= 'z'; ++k)
         cout <<setw(2)<< k << " ";		// Print all characters ‘a’ to ‘z’
     cout << endl;
 
     for (auto h : histogram)
-        cout << setw(2) << h << " ";		// Print histogram values
+        cout << setw(2) << h << " ";		// Print histogram values 
     cout << endl;
-
 }
 
 void rand_letters_generator() { //tej funkcji nie bedzie w programie
@@ -126,10 +137,9 @@ int main() {
     string catalog;
     cout << "Tell me catalog path, f.ex. 'C:\\Users\\admin\\abcd'  " << endl;
     cout<<"Type 'here' to start from current catalog" << endl;
-    //cin >> catalog;
-    catalog = "C:\\Users\\micha\\Documents\\testycpp"; //testing
-    cout << "Automaticaly changed" << endl;
-
+    cin >> catalog;
+    //catalog = "C:\\Users\\micha\\Documents\\testycpp"; //testing
+    
     reader* files = new reader(catalog);
     files->read(); //skanowanie w poszukiwaniu plikow
     vect fil_p=files->get_paths(); //odczytanie sciezek do programu glownego
@@ -139,11 +149,9 @@ int main() {
     cout<< "------------------------" << endl<<endl;
     
     scanner* analyze = new scanner(fil_p);
-    analyze->if_empty_check();
+    //analyze->if_empty_check();
     analyze->summary();
     analyze->letter_histogram();
-
-    Sleep(4000);
     cout << endl << endl;
     
     return 0;
